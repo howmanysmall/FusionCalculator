fb16041fa20176cbad87369ca97633ff540760c9
howmanysmall <lhamil29@bruinmail.slcc.edu>
2023-06-02 11:58:16 -0600

    remove binary

--- Utility/MathUtility.cs
+++ Utility/MathUtility.cs
@@ -12,35 +12,5 @@ namespace FusionCalculator.Utility
 
 			return a;
 		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int BinaryGcd(int a, int b)
-		{
-			if (b == 0)
-				return a;
-
-			if (a == 0)
-				return b;
-
-			if (a < 0)
-				a = Math.Abs(a);
-
-			if (b < 0)
-				b = Math.Abs(b);
-
-			if ((a & 1) == 0 && (b & 1) == 0)
-				return BinaryGcd(a >> 1, b >> 1) << 1;
-
-			if ((a & 1) == 0)
-				return BinaryGcd(a >> 1, b);
-
-			if ((b & 1) == 0)
-				return BinaryGcd(a, b >> 1);
-
-			if (a >= b)
-				return BinaryGcd((a - b) >> 1, b);
-
-			return BinaryGcd(a, (b - a) >> 1);
-		}
 	}
 }
